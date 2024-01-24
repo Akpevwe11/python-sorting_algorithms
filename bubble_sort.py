23552dc1f369e9a55ee391d2d9e5047472500613
Akpevwe <essiakpevwepraise@gmail.com>
2024-01-24 18:58:30 +0100

    sorted list

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def bubbleSort(arr):
+
+    for iter in range(len(arr)):
+        for index in range(0, len(arr) -1):
+            if arr[index] > arr[index + 1]:
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
+
+arr = [29, 10, 14, 37, 14]
+bubbleSort(arr)
+
+print(arr)
